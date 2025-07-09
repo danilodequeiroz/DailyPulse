f5034c501b49d8c820dd2675cf42b939752d9a20
Danilo de Queiroz <danilo.aq@gmail.com>
2025-07-09 04:24:06 -0300

    Implements ArticlesDataSource.kt.

---
+++ shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/ArticlesDataSource.kt
@@ -0,0 +1,56 @@
+package com.petros.efthymiou.dailypulse
+
+import com.petros.efthymiou.dailypulse.articles.data.network.model.ArticleRaw
+import petros.efthymiou.dailypulse.db.DailyPulseDatabase
+
+class ArticlesDataSource(
+    private val database: DailyPulseDatabase
+) {
+
+    fun getAllArticlesUsingExplicitLambda(): List<ArticleRaw> {
+        return database.dailyPulseDatabaseQueries.selectAllArticles(
+            mapper = { title, description, date, url ->
+                mapToArticlesRaw(
+                    title = title,
+                    desc = description,
+                    date = date,
+                    url = url
+                )
+            }
+        ).executeAsList()
+    }
+
+    fun getAllArticlesUsingTrailingLambda(): List<ArticleRaw> {
+        return database.dailyPulseDatabaseQueries.selectAllArticles { title, desc, date, imageUrl ->
+            mapToArticlesRaw(
+                title = title,
+                desc = desc,
+                date = date,
+                url = imageUrl
+            )
+        }.executeAsList()
+    }
+
+    fun getAllArticlesUsingCallableReference(): List<ArticleRaw> {
+        return database.dailyPulseDatabaseQueries.selectAllArticles(
+            ::mapToArticlesRaw
+        ).executeAsList()
+    }
+
+    fun mapToArticlesRaw(
+        title: String,
+        desc: String?,
+        date: String,
+        url: String?,
+    ): ArticleRaw {
+        return ArticleRaw(
+            title = title,
+            description = desc,
+            imageUrl = url,
+            date = date,
+
+            )
+    }
+
+
+}
--- shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/articles/data/network/model/ArticleRaw.kt
+++ shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/articles/data/network/model/ArticleRaw.kt
@@ -1,12 +1,12 @@
 package com.petros.efthymiou.dailypulse.articles.data.network.model
 
-import kotlinx.serialization.Serializable
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
 @Serializable
 data class ArticleRaw(
     @SerialName("title") var title: String,
     @SerialName("description") var description: String?,
-    @SerialName("urlToImage") var urlToImage: String?,
-    @SerialName("publishedAt") var publishedAt: String,
+    @SerialName("publishedAt") var date: String,
+    @SerialName("urlToImage") var imageUrl: String?,
 )
